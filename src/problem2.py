dc6d98d9a7e09ab2e7db5dbc374c7ced2dfaa7f7
zouh9907 <zouh@rose-hulman.edu>
2017-12-07 00:05:36 -0500

    finish problem a

--- src/problem2.py
+++ src/problem2.py
@@ -2,8 +2,8 @@
 PRACTICE Test 1, problem 2.
 
 Authors: David Mutchler, Valerie Galluzzi, Mark Hays, Amanda Stouder,
-         their colleagues and PUT_YOUR_NAME_HERE.
-"""  # TODO: 1. PUT YOUR NAME IN THE ABOVE LINE.
+         their colleagues and Huiruo Zou.
+"""  # DONE: 1. PUT YOUR NAME IN THE ABOVE LINE.
 
 import rosegraphics as rg
 
@@ -102,7 +102,7 @@ def problem2a(circle, rectangle, window):
       :type window:    rg.RoseWindow
     """
     # ------------------------------------------------------------------
-    # TODO: 2. Implement and test this function.
+    # DONE: 2. Implement and test this function.
     #          Tests have been written for you (above).
     # ------------------------------------------------------------------
     # ------------------------------------------------------------------
@@ -110,6 +110,28 @@ def problem2a(circle, rectangle, window):
     #    DIFFICULTY:      6
     #    TIME ESTIMATE:   10 to 15 minutes.
     # ------------------------------------------------------------------
+    circle.attach_to(window)
+
+    rectangle.attach_to(window)
+    window.render()
+    window.continue_on_mouse_click()
+
+    start = rectangle.get_upper_right_corner()
+    end = rectangle.get_lower_left_corner()
+
+    line = rg.Line(start, end)
+    line.arrow = 'last'
+
+    line.attach_to(window)
+
+    window.render()
+    window.continue_on_mouse_click()
+
+    circle.fill_color = rectangle.outline_color
+
+    window.render()
+    window.continue_on_mouse_click()
+
 
 def run_test_problem2b():
     """ Tests the  problem2b   function. """
